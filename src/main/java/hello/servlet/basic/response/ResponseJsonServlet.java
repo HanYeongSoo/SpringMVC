4b851049135337aca6ed474560e7e656dfbf9547
HanYeongSoo <86340380+HanYeongSoo@users.noreply.github.com>
2022-03-12 17:24:44 +0900

    11. HTTP 응답 데이터 - API JSON

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("han");
+        helloData.setAge(32);
+        // 이 값들을 {"username":"han", "age":32}로 json형태로 변환할 거임
+
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().println(result);
+    }
+}
